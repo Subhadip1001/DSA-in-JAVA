9d0394f01785cc9257f0f59b7cabbf9a9bcd0b1a
Subhadip Mandal <subhadipmandal714@gmail.com>
2025-06-07 08:57:25 +0530

    Single Element in a Sorted Array

---
+++ Searching/singleElement.java
@@ -0,0 +1,41 @@
+package Searching;
+
+// LeetCode: 540
+// Find the single non-duplicate element in a sorted array where every other element appears twice
+
+public class singleElement {
+    public static int searchSingle(int[] nums){
+        int n = nums.length;
+        if(n == 1) return nums[0];
+
+        int str = 0, end = n-1;
+        while(str<=end){
+            int mid = str+(end-str)/2;
+
+            if(nums[mid-1] != nums[mid] && nums[mid] != nums[mid+1]) return nums[mid];
+            // If mid is even
+            if(mid%2 == 0){
+                // 
+                if(nums[mid] == nums[mid-1]){
+                    end = mid-1;
+                }else{
+                    str = mid+1;
+                }
+            // If mid is odd   
+            }else{
+                if(nums[mid] == nums[mid-1]){
+                    str = mid+1;
+                }else{
+                    end = mid-1;
+                }
+            }
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+    //    int[] arr = {3,3,7,7,10,11,11};
+       int[] arr = {1,1,2,3,3,4,4,8,8};
+       int singleElement = searchSingle(arr);
+       System.out.println("Single element: " + singleElement);
+    }
+}
