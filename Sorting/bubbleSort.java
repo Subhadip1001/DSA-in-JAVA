5a8cbf2bdaf09365633792b183631ee63f06c6a7
Subhadip Mandal <subhadipmandal714@gmail.com>
2025-06-10 08:55:15 +0530

    Bubble Sort

--- Sorting/bubbleSort.java
+++ Sorting/bubbleSort.java
@@ -3,12 +3,17 @@ package Sorting;
 public class bubbleSort {
     public static void sorting(int[] arr, int n){
         for(int i=0; i<n; i++){
+            boolean swapped = false;
             for(int j=0; j<n-i-1; j++){
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    swapped = true;
                 }
+            }
+            if(!swapped) {
+                return;
             }
         }
     }
